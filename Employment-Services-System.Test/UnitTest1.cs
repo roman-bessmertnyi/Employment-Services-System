77dcc8c2c4677eb05221bf658cd69cb60497b442
roman.bessmertnyi <roman.bezsmerniy@gmail.com>
2018-01-10 18:04:26 +0200

    CI work

---
+++ Employment-Services-System.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Employment_Services_System.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
